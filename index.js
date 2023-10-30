6f24f40d5ea749274d47f6e2396facf1ae509f8d
Elijah <ekollie324@gmail.com>
2023-10-30 19:02:44 -0400

    variable lab

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+const divide = function (a, b) {
+  return a / b;
+};
 
-
-const divide = function(a, b){
-    return a/b;
-}
-
-const square = x => x ** 2
-
+const square = (x) => x ** 2;
 const add = (x, y) => x + y;
+git;
